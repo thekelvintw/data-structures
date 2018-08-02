ac685e95cfebe230ae6fba9f216063fe82225815
thekelvintw <kelvin.aiesec@gmail.com>
2018-08-02 15:35:17 -0700

    implement list add methods.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,26 +1,39 @@
 var LinkedList = function() {
   var list = {};
+  list.counter = 0;
   list.head = null;
   list.tail = null;
+  
 
   list.addToTail = function(value) {
+   // var node = Node(value);
+   var node = Node(value);
+      if (!list.head) {
+        list.head = node 
+        list.tail = node 
+      } else {
+        list.tail.next = node;
+        list.tail = node ;
+      }
   };
 
   list.removeHead = function() {
+
   };
 
   list.contains = function(target) {
   };
 
-  return list;
+  return list;  
+
 };
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
-  node.next = null;
-
+  node.next = null;//maybe need to change to link to next node
+  this.counter++;
+  
   return node;
 };
 
