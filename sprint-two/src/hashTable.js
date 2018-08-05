840028f9d7698f61f20a98beb9bff80f6f17119f
thekelvintw <kelvin.aiesec@gmail.com>
2018-08-04 17:33:37 -0700

    attempt to impletement hash table insert

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,7 +6,38 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+   
+  var innerArr = this._storage.get(index).set(index, []);
+  innerArr.push([k, v]);
+  
+  
+
+  
+
+  
+  
+// console.log(Array.isArray(this._storage) )
+  
+//   var bucket = this._storage.get(index);
+// if (!bucket) {
+// var tuples = bucket.set(index, []);
+// tuples.push([k, v]);
+
+
+// } else { 
+//     for (var i = 0; i < bucket.length; i++) {
+//       if (bucket[i][0] === k) {
+//         bucket[i][1] === v;
+//       } else {
+//         bucket.push([k, v]);
+//       }
+//     }
+   
+ // }      
+
 };
 
 HashTable.prototype.retrieve = function(k) {
